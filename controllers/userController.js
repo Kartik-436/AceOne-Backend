4ced05a088a3b565e03ae42ca6d3e0a3add0eded
Kartik Garg <gargkartik503@gmail.com>
2025-03-24 02:32:26 +0530

    payment integrtion complete for backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1324,6 +1324,7 @@ async function placeOrder(req, res) {
             payment: paymentResponse
         };
 
+        console.log("Payment response being sent:", paymentResponse);
         return sendResponse(res, 200, true, "Order placed successfully.", responseData);
     } catch (err) {
         console.error("Place Order Error:", err.message);
